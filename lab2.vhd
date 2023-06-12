9baa754f9285fb41100e587a6f55c0e477bb90f9
Nicolas Gagnier <ghikft@gmail.com>
2023-06-12 17:53:07 -0400

    vhdl qui synthetise

--- lab2.vhd
+++ lab2.vhd
@@ -3,7 +3,8 @@ use ieee.std_logic_1164.all;
 
 entity lab2 is
 	port (clock_50 		: in std_logic;
-			PS2_DAT			: in std_logic;
+			PS2_DAT			: inout std_logic;
+			key				: in std_logic_vector(3 downto 0);
 			vga_b 			: out std_logic_vector(7 downto 0);
 			vga_g 			: out std_logic_vector(7 downto 0);
 			vga_r 			: out std_logic_vector(7 downto 0);
@@ -12,14 +13,14 @@ entity lab2 is
 			VGA_HS 			: out std_logic;
 			VGA_SYNC_N 		: out std_logic;
 			VGA_VS 			: out std_logic;
-			PS2_CLK 			: out std_logic;
-			DRAM_CLK			: out std_logic_vector;
+			PS2_CLK 			: inout std_logic;
+			DRAM_CLK			: out std_logic;
 			DRAM_ADDR		: out std_logic_vector(12 downto 0);	
-			DRAM_BA			: out std_logic_vector(2 downto 0);
+			DRAM_BA			: out std_logic_vector(1 downto 0);
 			DRAM_CAS_N		: out std_logic;
 			DRAM_CKE			: out std_logic;
 			DRAM_CS_N		: out std_logic;
-			DRAM_DQ			: out std_logic_vector(15 downto 0);
+			DRAM_DQ			: inout std_logic_vector(15 downto 0);
 			DRAM_LDQM		: out std_logic;
 			DRAM_UDQM		: out std_logic;
 			DRAM_RAS_N		: out std_logic;
@@ -42,7 +43,7 @@ architecture structural of lab2 is
 			sdram_wires_cke               : out   std_logic;                                        --                        .cke
 			sdram_wires_cs_n              : out   std_logic;                                        --                        .cs_n
 			sdram_wires_dq                : inout std_logic_vector(63 downto 0) := (others => '0'); --                        .dq
-			sdram_wires_dqm               : out   std_logic_vector(7 downto 0);                     --                        .dqm
+			sdram_wires_dqm               : out   std_logic_vector(7 downto 0) := (others => '0');                     --                        .dqm
 			sdram_wires_ras_n             : out   std_logic;                                        --                        .ras_n
 			sdram_wires_we_n              : out   std_logic;                                        --                        .we_n
 			vga_controller_external_CLK   : out   std_logic;                                        -- vga_controller_external.CLK
@@ -59,8 +60,8 @@ architecture structural of lab2 is
 begin
 	nios_system : lab2arc
 	port map(
-		clk_clk => clock_50,
-		--reset_reset
+		clk_clk 								=> clock_50,
+		reset_reset 						=> key(3),
 		ps2_external_CLK 					=> PS2_CLK,
 		ps2_external_DAT 					=> PS2_DAT,
 		sdram_clk_clk 						=> DRAM_CLK,
@@ -70,7 +71,8 @@ begin
 		sdram_wires_cke 					=> DRAM_CKE,
 		sdram_wires_cs_n 					=> DRAM_CS_N,
 		sdram_wires_dq(15 downto 0) 	=> DRAM_DQ, --	pas un 64 bit de large
-		--sdram_wires_dqm 									Pas certain de ce qu'il faut assign la dedans
+		sdram_wires_dqm(0)				=> DRAM_LDQM, --Pas certain de ce qu'il faut assign la dedans
+		sdram_wires_dqm(1)				=> DRAM_UDQM,
 		sdram_wires_ras_n 				=> DRAM_RAS_N,
 		sdram_wires_we_n 					=> DRAM_WE_N,
 		vga_controller_external_CLK 	=> VGA_CLK,
@@ -82,10 +84,5 @@ begin
 		vga_controller_external_G 		=> vga_g,
 		vga_controller_external_B 		=> vga_b
 	);
-end structural
-		
-		
-		
-		
-		
+end structural;
 			
